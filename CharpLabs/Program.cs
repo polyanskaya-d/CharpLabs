5d3ecda1e998c2d7dd4080aac49247dc640029dc
polanska-d <polyanskaya00d@gmail.com>
2024-02-29 11:07:25 +0200

    init

---
+++ CharpLabs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CharpLabs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Pipi pupu puuuuuu");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CharpLabs
- 
+# CharpLabs
