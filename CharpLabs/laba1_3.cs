c16cd840dc3fb1a8c41fa0fa617ac9cf611402f5
polanska-d <polyanskaya00d@gmail.com>
2024-06-08 20:51:21 +0300

    laba1
    laba1

---
+++ CharpLabs/laba1_3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CharpLabs
+{
+	public class laba1_3
+	{
+		public laba1_3()
+		{
+		}
+	}
+}
+
---
+++ CharpLabs/laba1_4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CharpLabs
+{
+	public class laba1_4
+	{
+		public laba1_4()
+		{
+		}
+	}
+}
+
